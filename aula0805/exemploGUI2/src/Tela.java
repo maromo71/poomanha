d3deeda484a410782ad14247d6da520db96d2896
Maromo <professormoraes@gmail.com>
2025-05-08 12:30:33 -0300

    Aula do dia 08 de maio - Tela com Form Automatico

---
+++ aula0805/exemploGUI2/src/Tela.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Tela {
+    private JTextField txtValor;
+    private JTextField txtSaldo;
+    private JButton depositarButton;
+    private JButton sacarButton;
+    private JButton consultarButton;
+    private JButton sairButton;
+    private JTextArea txtMsg;
+    private JLabel lblValor;
+    private JLabel lblSaldo;
+}
