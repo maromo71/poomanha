88e6d572ff4c93c3e1cb2238c1c36ed385060f75
Maromo <professormoraes@gmail.com>
2026-08-25 11:41:19 -0300

    Aula do dia 25 de agosto

---
+++ aula2508/ProjetoConta/src/ContaTeste.java
@@ -0,0 +1,22 @@
+public class ContaTeste {
+    public Conta cc = new Conta();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void execCadastrar() {
+
+    }
+
+    public void execConsultar() {
+
+    }
+
+    public void execSacar() {
+
+    }
+    public void execDepositar(){
+
+    }
+}
